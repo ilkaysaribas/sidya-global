a99842b62215076ddc3569502c60f677477c1f0a
Sidya Global <info@sidyaglobal.com>
2026-06-09 01:23:28 +0300

    Bust B2B frontend cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sidya-global-v62";
+const CACHE_NAME = "sidya-global-v63";
 
 const SHELL_ASSETS = [
   "./",
@@ -6,7 +6,7 @@ const SHELL_ASSETS = [
   "./offline.html",
   "./styles.css?v=20260608-11",
   "./catalog-products.generated.js?v=20260608-2",
-  "./script.js?v=20260608-24",
+  "./script.js?v=20260609-1",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
   "./assets/app-icon-192.png",
