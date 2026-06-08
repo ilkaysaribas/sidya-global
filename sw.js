ceb82e52cf3e3a3d73050b18101c55ae05a0a0b6
Sidya Global <info@sidyaglobal.com>
2026-06-08 12:30:45 +0300

    Clarify B2B attachment email status

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sidya-global-v42";
+const CACHE_NAME = "sidya-global-v43";
 
 const SHELL_ASSETS = [
   "./",
@@ -6,7 +6,7 @@ const SHELL_ASSETS = [
   "./offline.html",
   "./styles.css?v=20260608-5",
   "./catalog-products.generated.js?v=20260601-5",
-  "./script.js?v=20260608-6",
+  "./script.js?v=20260608-7",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
   "./assets/app-icon-192.png",
