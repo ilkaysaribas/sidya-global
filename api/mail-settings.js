0c3ac5e8b2e54e8121aff7ab05300ffa40e14fd9
ilkaysaribas <ilkaysaribas@gmail.com>
2026-07-15 03:57:12 +0300

    fix: route mail settings through backend config

--- api/mail-settings.js
+++
@@ -1,6 +0,0 @@
-const backendConfigHandler = require("./backend-config");
-
-module.exports = async (req, res) => {
-  req.query = { ...(req.query || {}), mailCrm: "mail-settings" };
-  return backendConfigHandler(req, res);
-};
